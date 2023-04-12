2a505d2fecbab77c625dd2613d773732f19d60e2
Aidan Fitzpatrick <aidan.fitzpatrick@youlend.com>
2023-04-12 22:02:20 +0100

    feat: master: adding in random day if current day is weekend and bash equivalent

--- workout/workout.py
+++ workout/workout.py
@@ -5,6 +5,12 @@ import datetime
 # Get current day of the week as an integer (0 = Monday, 1 = Tuesday, etc.)
 day_of_week = datetime.datetime.today().weekday()
 
+# if today is a weekend day (5 = Saturday, 6 = Sunday)
+if day_of_week >= 5:
+    # choose a random weekday
+    random_day = random.randint(0, 4)  # 0 = Monday, 1 = Tuesday, ..., 4 = Friday
+    day_of_week = random_day
+
 # Define directory paths
 dir_paths = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
 
