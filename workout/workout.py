f14698288c1e5fefe564eac37e71e0591d54772a
Aidan Fitzpatrick <aidan.fitzpatrick@youlend.com>
2023-12-19 17:42:31 +0000

    fix: : refactored workout.py

--- workout/workout.py
+++ workout/workout.py
@@ -4,30 +4,47 @@ import datetime
 import argparse
 import os
 
-def print_random_lines(directory, file_count, title, delimiter=None):
-    """Print a random line from each file in the specified directory."""
-    print(f"\n{title}")
-    print("-" * len(title))
+def get_filtered_filenames(directory):
+    """Return filenames without digits and with underscores replaced."""
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    non_digit_files = [f for f in files if not f.isdigit()]
+    return [subpart for f in non_digit_files for subpart in f.split('_')]
+
+def create_formatted_title(split_files, title):
+    """Create a formatted title string with the first file element prefixed."""
+    if split_files:
+        split_files[0] = " - " + split_files[0]
+        formatted_title = ', '.join(split_files[:-1]) + ' and ' + split_files[-1]
+    else:
+        formatted_title = ''
+    return f"{title}{formatted_title}"
+
+def print_exercise_routine(directory, file_count, title):
+    """Print a random line from each file in the specified directory and the formatted title."""
+    split_files = get_filtered_filenames(directory)
+    formatted_title = create_formatted_title(split_files, title)
+
+    print(f"\n{formatted_title}")
+    print("-" * len(formatted_title))
 
     for i in range(file_count):
-        with open(f"{directory}/{i}", "r") as f:
-            line = random.choice(f.readlines()).strip()
-            print(line)
+        try:
+            with open(f"{directory}/{i}", "r") as file:
+                line = random.choice(file.readlines()).strip()
+                print(line)
+        except FileNotFoundError:
+            print(f"File not found: {directory}/{i}")
 
-def main():
-    """Main function to execute the script."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--day", help="Specify the day of the week.")
-    args = parser.parse_args()
-
+def determine_day_of_week(args):
+    """Determine the day of the week based on arguments."""
+    days = ['monday', 'teusday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     day_of_week = datetime.datetime.today().weekday()
-    if day_of_week >= 5:
+
+    if day_of_week >= 5:  # For weekends, choose a random weekday
         day_of_week = random.randint(0, 4)
 
     if args.day:
         day_arg = args.day.lower()
-        days = ['monday', 'teusday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-
         if day_arg == "t":
             day_of_week = (day_of_week + 1) % 7
         elif day_arg == "r":
@@ -35,13 +52,26 @@ def main():
         elif day_arg in days:
             day_of_week = days.index(day_arg)
         else:
-            print("Invalid day argument.")
-            exit(1)
+            raise ValueError("Invalid day argument.")
+    
+    return day_of_week
 
-    dir_paths = ['monday', 'teusday', 'wednesday', 'thursday', 'friday']
-    print_random_lines("cardio", 6, "Cardio")
-    print_random_lines(dir_paths[day_of_week], 5, "Circuit Training")
-    print_random_lines("mobility", 2, "Mobility and Flexibility Exercises")
+def main():
+    """Main function to execute the script."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--day", help="Specify the day of the week.")
+    args = parser.parse_args()
+
+    try:
+        day_of_week = determine_day_of_week(args)
+    except ValueError as e:
+        print(e)
+        exit(1)
+
+    weekly_routines = ['monday', 'teusday', 'wednesday', 'thursday', 'friday']
+    print_exercise_routine("cardio", 6, "Cardio")
+    print_exercise_routine(weekly_routines[day_of_week], 5, "Circuit Training")
+    print_exercise_routine("mobility", 2, "Mobility and Flexibility Exercises")
 
 if __name__ == "__main__":
     main()
